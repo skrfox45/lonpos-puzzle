a97b98352f6959bcb44ee3b8e16f32df3de3726b
skrfox45 <skrfox45@gmail.com>
2026-09-17 20:40:26 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE="lonpos-retired-v7";
-self.addEventListener("install",e=>e.waitUntil(self.skipWaiting()));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>e.respondWith(fetch(e.request)));
